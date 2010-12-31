ba36a24901b6f95217a36a729cb1f477a294502e
Robin Houston <robin.houston@gmail.com>
2010-12-31 16:41:53 +0000

    First basically-complete version

--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 aws put -v "Content-type: text/html; charset=utf-8" "x-amz-acl:public-read" \
-    s3.boskent.com/mazes/eller.html eller.html
+    s3.boskent.com/mazes/race.html race.html
+
+# aws put -v "Content-type: text/html; charset=utf-8" "x-amz-acl:public-read" \
+#     s3.boskent.com/mazes/flipper.html flipper.html
