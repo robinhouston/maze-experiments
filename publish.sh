2530599296429016b3c96ab3c08dfce90d4d5663
Robin Houston <robin.houston@gmail.com>
2011-01-09 21:07:10 +0000

    Make publish.sh generic, so the files to be published are passed as arguments: use publish.sh * to publish all files.

--- publish.sh
+++ publish.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-aws put -v "Content-type: text/html; charset=utf-8" "x-amz-acl:public-read" \
-    s3.boskent.com/mazes/race.html race.html
-
-# aws put -v "Content-type: text/html; charset=utf-8" "x-amz-acl:public-read" \
-#     s3.boskent.com/mazes/flipper.html flipper.html
-
-aws put -v "Content-type: text/html; charset=utf-8" "x-amz-acl:public-read" \
-    s3.boskent.com/mazes/kruskal.html kruskal.html
+while [ $# -gt 0 ]; do
+   case "$1" in
+       *.html)
+          aws put -v "Content-type: text/html; charset=utf-8" "x-amz-acl:public-read" \
+              s3.boskent.com/mazes/"$1" "$1"
+          ;;
+       *.js)
+          aws put -v "Content-type: text/javascript; charset=utf-8" "x-amz-acl:public-read" \
+              s3.boskent.com/mazes/"$1" "$1"
+          ;;
+       *.css)
+          aws put -v "Content-type: text/css; charset=utf-8" "x-amz-acl:public-read" \
+              s3.boskent.com/mazes/"$1" "$1"
+          ;;
+       *)
+         echo >&2 "Filename '$1' has an unknown extension; not published."
+         ;;
+   esac
+   shift
+done
